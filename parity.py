c92e714b68c354748b1d3fbb5186403f7b3061c3
ashenoy95 <shenoyaniket95@gmail.com>
2017-04-25 19:25:31 -0400

    Update parity.py

--- parity.py
+++ parity.py
@@ -5,10 +5,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def phi(v):													#sigmoid func
+def phi(v):										#sigmoid func
 	return 1/(1+np.exp(-v))
 
-def backprop(eta):											#online backprop
+def backprop(eta):									#online backprop
 	#i/p truth table for 4 bit parity checker
 	X = np.array([	[0,0,0,0,1],
 					[0,0,0,1,1],
@@ -26,7 +26,7 @@ def backprop(eta):											#online backprop
 					[1,1,0,1,1],
 					[1,1,1,0,1],
 					[1,1,1,1,1]])
-	D = np.array([[0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0]]).T 	#desired o/p
+	D = np.array([[0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0]]).T 				#desired o/p
 
 	#randomly initialized weights b/w -1 & 1 (0 mean)
 	w_ij = np.random.uniform(-1,1,(4,5))
@@ -40,10 +40,10 @@ def backprop(eta):											#online backprop
 	print('\neta:',eta)
 	
 	while True:
-		diff = []											#stores abs error (diff) for all i/p patterns in current epoch	
-		for i in range(X.shape[0]):							#online update
+		diff = []								#stores abs error (diff) for all i/p patterns in current epoch	
+		for i in range(X.shape[0]):						#online update
 			x = np.reshape(X[i,],(5,1)) 					#single i/p pattern
-			d = D[i]										#desired o/p corresponding to i/p pattern
+			d = D[i]							#desired o/p corresponding to i/p pattern
 
 			#i: i/p layer
 			#j: hidden layer
@@ -58,7 +58,7 @@ def backprop(eta):											#online backprop
 			e_k = d - y_k
 
 			#backprop 
-			delta_k = e_k*y_k*(1-y_k)						#delta rule
+			delta_k = e_k*y_k*(1-y_k)					#delta rule
 			delta_j = w_jk.T*delta_k*y_j*(1-y_j)
 			
 			#weight updation
@@ -67,15 +67,15 @@ def backprop(eta):											#online backprop
 			w_ij+= delta_w_ij
 			w_jk+= delta_w_jk
 
-			diff.append(e_k[0,0])							#appending error for current i/p pattern
+			diff.append(e_k[0,0])						#appending error for current i/p pattern
 
-		epoch+=1											#epoch complete
+		epoch+=1								#epoch complete
 		error = np.max(np.abs(diff))						#abs error (diff) in current epoch
-		if epoch%10000==0:									#printing error after every 10000 epochs
+		if epoch%10000==0:							#printing error after every 10000 epochs
 			print('epoch:',epoch,'\nerror:',error,'\n')
-		if error<=.05:										#convergenve criteria
+		if error<=.05:								#convergenve criteria
 			break
-		#np.random.shuffle(X)								#shuffle i/p patterns for next epoch (helps to avoid local minima)
+		#np.random.shuffle(X)							#shuffle i/p patterns for next epoch (helps to avoid local minima)
 
 	print('epoch:',epoch,'\nerror:',error,'\n')
 	return epoch
